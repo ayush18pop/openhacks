a0d276db6e8d4e6336f5e930839d9b5b47cf9acc
prktypes <prakkharong@gmail.com>
2025-08-19 18:02:00 +0530

    refactor: enhance layout structure and improve component organization in Home and Background components

--- components/landing/Background.tsx
+++ components/landing/Background.tsx
@@ -6,7 +6,7 @@ import { cn } from "../../lib/utils";
 export const BoxesCore = ({ className, ...rest }: { className?: string }) => {
   const rows = new Array(150).fill(1);
   const cols = new Array(100).fill(1);
-  let colors = [
+  const colors = [
     "#93c5fd",
     "#f9a8d4",
     "#86efac",
@@ -17,9 +17,8 @@ export const BoxesCore = ({ className, ...rest }: { className?: string }) => {
     "#a5b4fc",
     "#c4b5fd",
   ];
-  const getRandomColor = () => {
-    return colors[Math.floor(Math.random() * colors.length)];
-  };
+
+  const getRandomColor = () => colors[Math.floor(Math.random() * colors.length)];
 
   return (
     <div
@@ -27,27 +26,24 @@ export const BoxesCore = ({ className, ...rest }: { className?: string }) => {
         transform: `translate(-40%,-60%) skewX(-48deg) skewY(14deg) scale(0.675) rotate(0deg) translateZ(0)`,
       }}
       className={cn(
-        "absolute -top-1/4 left-1/4 z-0 flex h-full w-full -translate-x-1/2 -translate-y-1/2 p-4",
-        className,
+        "absolute -top-1/4 left-1/4 z-0 flex h-full w-full -translate-x-1/2 -translate-y-1/2 p-4 pointer-events-none",
+        className
       )}
       {...rest}
     >
       {rows.map((_, i) => (
-        <motion.div
-          key={`row` + i}
-          className="relative h-8 w-16 border-l border-slate-700"
-        >
+        <motion.div key={`row${i}`} className="relative h-8 w-16 border-l border-slate-700 pointer-events-none">
           {cols.map((_, j) => (
             <motion.div
               whileHover={{
-                backgroundColor: `${getRandomColor()}`,
+                backgroundColor: getRandomColor(),
                 transition: { duration: 0 },
               }}
               animate={{
                 transition: { duration: 2 },
               }}
-              key={`col` + j}
-              className="relative h-8 w-16 border-t border-r border-slate-700"
+              key={`col${j}`}
+              className="relative h-8 w-16 border-t border-r border-slate-700 pointer-events-none"
             >
               {j % 2 === 0 && i % 2 === 0 ? (
                 <svg
@@ -56,13 +52,9 @@ export const BoxesCore = ({ className, ...rest }: { className?: string }) => {
                   viewBox="0 0 24 24"
                   strokeWidth="1.5"
                   stroke="currentColor"
-                  className="pointer-events-none absolute -top-[14px] -left-[22px] h-6 w-10 stroke-[1px] text-slate-700"
+                  className="absolute -top-[14px] -left-[22px] h-6 w-10 stroke-[1px] text-slate-700 pointer-events-none"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 6v12m6-6H6"
-                  />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
                 </svg>
               ) : null}
             </motion.div>
